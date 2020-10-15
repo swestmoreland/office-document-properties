5aacaa9031a8014acd9d7b6d2c778ad70d6f6ea3
Steven Westmoreland <steven.westmoreland@gmail.com>
2020-10-15 10:35:07 -0500

    Updated tests.

--- test/test.js
+++ test/test.js
@@ -10,31 +10,44 @@ describe('getDocumentProperties', function() {
         expect(getDocumentProperties).to.be.an.instanceof(Object);
     });
 
-    it('properties should be functions', function() {
-        expect(typeof getDocumentProperties.fromFilePath).to.eql('function');
-        expect(typeof getDocumentProperties.fromBuffer).to.eql('function');
+    var testFiles = ['test.docx', 'test.pptx', 'test.xlsx', 'google.docx', 'google.pptx', 'google.xlsx'];
+
+    var test = function(done) {
+        return function(error, data) {
+            expect(error).to.be.null;
+            expect(data).to.be.an('object');
+            done();
+        };
+    };
+
+    describe('#fromBuffer()', function() {
+
+        it('should be a function', function() {
+            expect(typeof getDocumentProperties.fromBuffer).to.eql('function');
+        });
+
+        testFiles.forEach(function(file) {
+            it(`fromBuffer(${path.join(__dirname, 'files', file)}, callback)`, function(done) {
+                var filePath = path.join(__dirname, 'files', file),
+                    fileBuff = fs.readFileSync(filePath);
+                getDocumentProperties.fromBuffer(fileBuff, test(done));
+            });
+        })
+
     });
 
-    describe('can handle all the different API variations', function() {
+    describe('#fromFilePath()', function() {
 
-        var test = function(done) {
-            return function(error, data) {
-                expect(error).to.be.null;
-                expect(data).to.be.an('object');
-                done();
-            };
-        };
-
-        it('fromFilePath(filePath, callback)', function(done) {
-            var filePath = path.join(__dirname, 'files', 'test.docx');
-            getDocumentProperties.fromFilePath(filePath, test(done));
+        it('should be a function', function() {
+            expect(typeof getDocumentProperties.fromFilePath).to.eql('function');
         });
 
-        it('fromBuffer(buffer, callback)', function(done) {
-            var filePath = path.join(__dirname, 'files', 'test.docx'),
-                fileBuff = fs.readFileSync(filePath);
-            getDocumentProperties.fromBuffer(fileBuff, test(done));
-        });
+        testFiles.forEach(function(file) {
+            it(`fromFilePath(${path.join(__dirname, 'files', file)}, callback)`, function(done) {
+                var filePath = path.join(__dirname, 'files', file);
+                getDocumentProperties.fromFilePath(filePath, test(done));
+            });
+        })
 
     });
 
